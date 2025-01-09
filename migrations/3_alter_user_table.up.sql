2b2d9beb821576358243793a80fdccec1749b7d4
Jesse Nicholas <42322303+JesseNicholas00@users.noreply.github.com>
2025-01-10 00:28:38 +0800

    added missing fieldsclear

--- migrations/3_alter_user_table.up.sql
+++ migrations/3_alter_user_table.up.sql
@@ -1,3 +1,4 @@
 ALTER TABLE users ADD IF NOT EXISTS user_image_uri TEXT;
 ALTER TABLE users ADD IF NOT EXISTS company_name TEXT;
+ALTER TABLE users ADD IF NOT EXISTS user_name TEXT;
 ALTER TABLE users ADD IF NOT EXISTS company_image_uri TEXT;
