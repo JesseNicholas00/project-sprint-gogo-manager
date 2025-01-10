7bcfc7f9cb44377ce31cc9edd2f5e05bc6178db9
j03hanafi <namoapak14@gmail.com>
2025-01-11 02:01:41 +0700

    Added query for check isContainEmployee in departmentRepo

--- repos/department/is_contain_employee.go
+++ repos/department/is_contain_employee.go
@@ -2,9 +2,27 @@ package department
 
 import (
 	"context"
+	"github.com/JesseNicholas00/GogoManager/utils/errorutil"
 	"github.com/google/uuid"
 )
 
 func (repo *departmentRepositoryImpl) IsContainEmployee(ctx context.Context, departmentID uuid.UUID) (bool, error) {
-	return true, nil
+	if err := ctx.Err(); err != nil {
+		return false, err
+	}
+
+	ctx, sess, err := repo.dbRizzer.GetOrNoTx(ctx)
+	if err != nil {
+		err = errorutil.AddCurrentContext(err)
+		return false, err
+	}
+
+	var count int
+	err = sess.Stmt(ctx, repo.statements.isContainEmployee).QueryRowxContext(ctx, departmentID).Scan(&count)
+	if err != nil {
+		err = errorutil.AddCurrentContext(err)
+		return false, err
+	}
+
+	return count > 0, nil
 }
